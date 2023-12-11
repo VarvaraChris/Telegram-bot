f8853294532ed02cb181d0d5af52c3c084e69f1a
Родионова Варвара Олеговна <65398336+VarvaraChris@users.noreply.github.com>
2023-12-11 21:41:15 +0300

    Update config_reader.py

--- src/config_reader.py
+++ src/config_reader.py
@@ -1,10 +1,9 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 from pydantic import SecretStr
 
 #храним названия словарей и направление на токен
 class Settings(BaseSettings):
     bot_token: SecretStr
-    model_config = SettingsConfigDict(env_file='src/.env', env_file_encoding='utf-8')
 
 name_dict_eng = "Dictionaries/EngDict.json"
 name_dict_ch = "Dictionaries/ChDict.json"
